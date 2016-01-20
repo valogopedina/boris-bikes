5c9b3c35894b274d2adba2e1424b31f8a432082f
valogopedina <valentina.monetta@tiscali.it>
2016-01-20 15:46:29 +0000

    completed challenge 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
+	#it 'respond_to release_bike' do 
+		#expect(DockingStation).to responde_to(:release_bike)
+	#end
+	it { is_expected.to respond_to(:release_bike)}
 
 end
